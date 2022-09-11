6153bce8708809d35de6637f9314512ccfab7bed
rafiudd <rayhanrafiudd@gmail.com>
2022-09-11 10:30:17 +0700

    Fix: Logo URL

--- src/components/_module/ModuleNavbarLanding.vue
+++ src/components/_module/ModuleNavbarLanding.vue
@@ -126,8 +126,8 @@ export default {
     },
     logo() {
         return this.scrollPosition > 98
-               ? require('/Users/rafiudd/Documents/code-playground/Project_haji/src/assets/images/colored_logo.svg')
-               : require('/Users/rafiudd/Documents/code-playground/Project_haji/src/assets/images/logo.svg');
+               ? require('https://res.cloudinary.com/duh8crgew/image/upload/v1662866966/lumbungdesa/images/logo/colored_logo_trdx7e.svg')
+               : require('https://res.cloudinary.com/duh8crgew/image/upload/v1662866961/lumbungdesa/images/logo/logo_potrhm.svg');
     }
   },
 
